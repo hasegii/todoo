d77c71eae353116a2aa9cd7113b10bbcc3b8e60b
hasegii <zxc.perm@mail>
2024-05-29 11:09:33 +0500

    second

---
+++ lib/home.dart
@@ -0,0 +1,6 @@
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
---
+++ lib/todo_item.dart
@@ -0,0 +1,12 @@
+class ToDoItem extends StatelessWidget {
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
+
+  const ToDoItem({
+    Key? key,
+    required this.todo,
+    required this.onToDoChanged,
+    required this.onDeleteItem,
+  }) : super(key: key);
+}
